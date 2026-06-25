a64498b248a1e3ef68974670321d8f977dd7e9da
divyansh tyagi <divyanshtyagi502@gmail.com>
2026-06-25 22:55:52 +0530

    Implement JWT authentication and protected routes

--- backend/app/api/user.py
+++ backend/app/api/user.py
@@ -1,15 +1,62 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-
+from app.core.auth import get_current_user
 from app.db.dependencies import get_db
 from app.models.user import User
-from app.schemas.user import UserCreate, UserResponse
+from app.schemas.user import UserCreate, UserResponse, UserLogin
 from app.core.security import hash_password
-
+from app.schemas.token import Token
+from app.core.security import (
+    hash_password,
+    verify_password,
+    create_access_token,
+)
 router = APIRouter()
+
+@router.get("/me")
+def get_me(
+    current_user=Depends(get_current_user)
+):
+    return {
+        "id": current_user.id,
+        "username": current_user.username,
+        "email": current_user.email
+    }
+
+@router.post("/login", response_model=Token)
+def login(
+    user: UserLogin,
+    db: Session = Depends(get_db)
+):
+    db_user = (
+        db.query(User).filter(User.email==user.email).first()
+    )
+    if not db_user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email or password"
+        )
+    if not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email or password"
+        )
+    access_token = create_access_token(
+        data={"sub": db_user.email}
+    )
+    return Token(access_token=access_token, token_type="bearer"
+)
 
 @router.post("/register", response_model=UserResponse)
 def register_user(user: UserCreate, db: Session = Depends(get_db)):
+    existing_user = (
+        db.query(User).filter(User.email==user.email).first()
+    )
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User with this email already exists"
+        )
     new_user = User(
         username=user.username,
         email=user.email,
