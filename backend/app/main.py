540cef12628569a53f28653061007a03eb9d4511
divyansh tyagi <divyanshtyagi502@gmail.com>
2026-06-25 20:16:09 +0530

    Setup database and user model

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,8 @@
 from fastapi import FastAPI
+from app.db.database import engine, Base
+from app.models.user import User
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Instagram Clone MVP")
 
